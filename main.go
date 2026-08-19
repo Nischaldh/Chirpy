f332f43ac3d77428c0c57a8c976a0079af871959
Nischal Dhungana <nischaldhungana10@gmail.com>
2026-08-19 05:35:02 +0000

    Implement simple server in go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	
+	"net/http"
+	
+)
+
+
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	const port = "8080"
+	mux := http.NewServeMux()
+	
+	ser := &http.Server{
+		Addr: ":"+port,
+		Handler: mux,
+	}
+
+	log.Fatal(ser.ListenAndServe())
 }
